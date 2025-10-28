1df9fe828f24578c8a6c4cc0ab6959419e739383
Yvan Ngalle <nycorpdev@gmail.com>
2025-10-28 18:18:35 +0100

    feat(dynamic wallet): now we can create wallet as desire

--- src/Models/GatewayPayload.php
+++ src/Models/GatewayPayload.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Str;
 
 class GatewayPayload
 {
+    private const PREFIX = "#___";
     protected ?string $orderId = null;
     protected ?float $amount = null;
     protected ?string $accountNumber = null;
@@ -18,72 +19,11 @@ class GatewayPayload
     protected ?string $paymentMethod = null;
     protected ?string $providerResponse = null; // e.g. "mobile_money", "card"
     protected ?array $metadata = [];
+    protected ?string $callbackUrl = null;
 
     public static function make(): static
     {
         return new static();
-    }
-
-    public function setOrderId(string $orderId): static
-    {
-        $this->orderId = $orderId;
-        return $this;
-    }
-
-    public function setAmount(float $amount): static
-    {
-        $this->amount = $amount;
-        return $this;
-    }
-
-    // ───── Fluent Setters ─────
-
-    public function setAccountNumber(string $accountNumber): static
-    {
-        $this->accountNumber = $accountNumber;
-        return $this;
-    }
-
-    public function setAccountName(string $accountName): static
-    {
-        $this->accountName = $accountName;
-        return $this;
-    }
-
-    public function setPhoneNumber(string $phoneNumber): static
-    {
-        $this->phoneNumber = $phoneNumber;
-        return $this;
-    }
-
-    public function setAccountEmail(string $accountEmail): static
-    {
-        $this->accountEmail = $accountEmail;
-        return $this;
-    }
-
-    public function setReference(string $reference): static
-    {
-        $this->reference = $reference;
-        return $this;
-    }
-
-    public function setPaymentMethod(string $paymentMethod): static
-    {
-        $this->paymentMethod = $paymentMethod;
-        return $this;
-    }
-
-    public function setProviderResponse(string $providerResponse): static
-    {
-        $this->providerResponse = $providerResponse;
-        return $this;
-    }
-
-    public function setMetadata(array $metadata): static
-    {
-        $this->metadata = $metadata;
-        return $this;
     }
 
     public function load(): static
@@ -91,26 +31,13 @@ class GatewayPayload
         return $this->fill(request()->request->all());
     }
 
-    public function setAccountCity(?string $accountCity): GatewayPayload
-    {
-        $this->accountCity = $accountCity;
-        return $this;
-    }
-
-    public function setAccountCountry(?string $accountCountry): GatewayPayload
-    {
-        $this->accountCountry = $accountCountry;
-        return $this;
-    }
-
-
     /**
      * Mass assign properties from array
      */
     public function fill(array $data): static
     {
         foreach ($data as $key => $value) {
-            $property = Str::camel($key);
+            $property = Str::replaceFirst(self::PREFIX,'',Str::camel($key));
             if (property_exists($this, $property)) {
                 $this->{$property} = $value;
             }
@@ -125,9 +52,30 @@ class GatewayPayload
     }
 
     /**
-     * Return all fields as associative array
+     * Automatically convert all class properties to associative array
+     * @return array
      */
     public function toArray(): array
+    {
+        $data = [];
+
+        // Get only non-static properties of the current class
+        $properties = (new \ReflectionClass($this))->getProperties(\ReflectionProperty::IS_PROTECTED);
+
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $name = Str::snake($property->getName());
+            $key = self::PREFIX . $name;
+            $data[$key] = $property->getValue($this);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Return all fields as associative array
+     */
+    public function toArrays(): array
     {
         return [
             'order_id' => $this->orderId,
@@ -150,9 +98,21 @@ class GatewayPayload
         return $this->orderId;
     }
 
+    public function setOrderId(string $orderId): static
+    {
+        $this->orderId = $orderId;
+        return $this;
+    }
+
     public function getAmount(): ?float
     {
         return $this->amount;
+    }
+
+    public function setAmount(float $amount): static
+    {
+        $this->amount = $amount;
+        return $this;
     }
 
     public function getAccountNumber(): ?string
@@ -160,9 +120,21 @@ class GatewayPayload
         return $this->accountNumber;
     }
 
+    public function setAccountNumber(string $accountNumber): static
+    {
+        $this->accountNumber = $accountNumber;
+        return $this;
+    }
+
     public function getAccountName(): ?string
     {
         return $this->accountName;
+    }
+
+    public function setAccountName(string $accountName): static
+    {
+        $this->accountName = $accountName;
+        return $this;
     }
 
     public function getPhoneNumber(): ?string
@@ -170,9 +142,21 @@ class GatewayPayload
         return $this->phoneNumber;
     }
 
+    public function setPhoneNumber(string $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+        return $this;
+    }
+
     public function getAccountEmail(): ?string
     {
         return $this->accountEmail;
+    }
+
+    public function setAccountEmail(string $accountEmail): static
+    {
+        $this->accountEmail = $accountEmail;
+        return $this;
     }
 
     public function getReference(): ?string
@@ -180,9 +164,21 @@ class GatewayPayload
         return $this->reference;
     }
 
+    public function setReference(string $reference): static
+    {
+        $this->reference = $reference;
+        return $this;
+    }
+
     public function getPaymentMethod(): ?string
     {
         return $this->paymentMethod;
+    }
+
+    public function setPaymentMethod(string $paymentMethod): static
+    {
+        $this->paymentMethod = $paymentMethod;
+        return $this;
     }
 
     public function getProviderResponse(): ?string
@@ -190,9 +186,21 @@ class GatewayPayload
         return $this->providerResponse;
     }
 
+    public function setProviderResponse(string $providerResponse): static
+    {
+        $this->providerResponse = $providerResponse;
+        return $this;
+    }
+
     public function getMetadata(): ?array
     {
         return $this->metadata;
+    }
+
+    public function setMetadata(array $metadata): static
+    {
+        $this->metadata = $metadata;
+        return $this;
     }
 
     public function getAccountCity(): ?string
@@ -200,9 +208,32 @@ class GatewayPayload
         return $this->accountCity;
     }
 
+    public function setAccountCity(?string $accountCity): GatewayPayload
+    {
+        $this->accountCity = $accountCity;
+        return $this;
+    }
+
     public function getAccountCountry(): ?string
     {
         return $this->accountCountry;
+    }
+
+    public function setAccountCountry(?string $accountCountry): GatewayPayload
+    {
+        $this->accountCountry = $accountCountry;
+        return $this;
+    }
+
+    public function getCallbackUrl(): ?string
+    {
+        return $this->callbackUrl;
+    }
+
+    public function setCallbackUrl(?string $callbackUrl): GatewayPayload
+    {
+        $this->callbackUrl = $callbackUrl;
+        return $this;
     }
 
 
